a14b797a633f597e3fdd21e8e40f345562031c90
AliTakrar <tekrar.co@gmail.com>
2022-01-22 19:42:23 +0330

    agreez initial

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,86 @@
 <script>
-	export let name;
+  import Header from "./UI/Header.svelte";
+  import AgreezGrid from "./Features/AgreezGrid.svelte";
+
+  let title = '';
+  let subtitle = ''; 
+  let address = '';
+  let email = '';
+  let description = '';
+  let imageUrl = '';
+
+  let agreezs = [
+    {
+      id: "a1",
+      title: "Programing Languages",
+      subtitle: "Agreez what programing languages are suited for our needs",
+      description:
+        "In this agreez , we will have some experts in frontend and backend programing languages",
+      imageUrl: "https://miro.medium.com/max/1400/1*L5QyrMNalM3yhtgdgBcvkQ.png",
+      address: "26th Seraj str , Shiraz",
+      contactEmail: "pl@test.com",
+    },
+    {
+      id: "a2",
+      title: "Frameworks",
+      subtitle: "Agreez what frameworks are suited for our needs",
+      description: "In this agreez , we will have some experts in frameworks",
+      imageUrl: "https://miro.medium.com/max/2000/1*ItGd7Itb99Cr6rE4qVD8wg.png",
+      address: "12th Eram blv , Shiraz",
+      contactEmail: "fr@test.com",
+    },
+  ];
+
+  function addAgreez(){
+    const newAgreez = {
+      id: Math.random().toString(),
+      title: title ,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contactEmail: email,
+      address: address
+    };
+    agreezs = [newAgreez,...agreezs];
+  };
 </script>
 
+<Header />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <form on:submit|preventDefault="{addAgreez}">
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value="{title}"/>
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value="{subtitle}"/>
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value="{address}"/>
+    </div>
+    <div class="form-control">
+      <label for="imageUrl">Image URL</label>
+      <input type="text" id="imageUrl" bind:value="{imageUrl}"/>
+    </div>
+    <div class="form-control">
+      <label for="email">E-Mail</label>
+      <input type="email" id="email" bind:value="{email}"/>
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea rows="3" id="description" bind:value="{description}"/>
+    </div>
+    <button type="submit">Save</button>
+  </form>
+
+  <AgreezGrid {agreezs} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    margin-top: 5rem;
+  }
 </style>
---
+++ src/Features/AgreezGrid.svelte
@@ -0,0 +1,31 @@
+<script>
+  import AgreezItem from "./AgreezItem.svelte";
+  export let agreezs;
+</script>
+
+<style>
+    section{
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-gap: 1rem;
+    }
+    @media (min-width: 768px){
+        section {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
+
+<section id="agreezs">
+  {#each agreezs as agreez}
+    <AgreezItem
+      title={agreez.title}
+      subtitle={agreez.subtitle}
+      imageUrl={agreez.imageUrl}
+      description={agreez.description}
+      email={agreez.contactEmail}
+      address={agreez.address}
+    />
+  {/each}
+</section>
